35347229f9b0935a709b18df04258e9d372b4ec9
CAD Client <CAD Client>
2016-04-01 16:06:21 +0900

    예제5 완료

---
+++ ex05/ex05.02.py
@@ -0,0 +1,14 @@
+name = "Son seungmin"
+my_age = 23
+my_height_cm = 171
+cm_to_inch = 1.0 / 2.54
+my_height_inch = my_height_cm * cm_to_inch
+my_weight_kg = 80
+my_eyes = 'Brown'
+my_hair = 'Black'
+
+print ("Let's Talk about %s." %name)
+print ("He's %g centimeters tall." %my_height_cm)
+print ("He's %g inches tall." %my_weight_kg)
+print ("Actually that's not too heavy.")
+print ("He's got %s eyes and %s depending on the coffee.")
