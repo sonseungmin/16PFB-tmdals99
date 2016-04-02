35a1585b7a655d6456a3928146f981caadb3866d
CAD Client <CAD Client>
2016-04-02 12:12:35 +0900

    ex11 idle's complete

---
+++ ex11/ex11.py
@@ -0,0 +1,9 @@
+print ("How old are you?")
+age = raw_input(3)
+print ("How tall are you?")
+height = raw_input(2)
+print ("How much do you weight?")
+weight = raw_input(1)
+
+print "So, you're %r old, %r tall and %r heavy. % (age, height, weight)
+       
