dfedb13a4b17a316493dcef7e3644268debe552b
CAD Client <CAD Client>
2016-04-02 11:59:12 +0900

    맨 밑에 쓴 명령문이 맨밑에 일렬로 나란히 할수 있게 해줌

---
+++ ex10/ex10.py
@@ -0,0 +1,19 @@
+tabby_cat = "\tI'm tabbed in.한글출력"
+persian_cat = "    I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+space_cat = "\t I'm tabbed in 한글출력"
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print (tabby_cat)
+print(space_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
+print ('Study Drills 10.05\n %r' % fat_cat)
+
+
