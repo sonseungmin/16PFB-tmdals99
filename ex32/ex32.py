5d50811a604ec14e33a956f34be4b02ef242e4d1
CAD Client <CAD Client>
2016-05-20 16:02:40 +0900

    30,31,32 차이점 비교하기

---
+++ ex32/ex32.py
@@ -0,0 +1,38 @@
+# -*-coding:utf8
+# http://learnpythonthehardway.org/bok/ex32.html
+
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+for number in the_count:
+    print("This is count %d" % number)
+
+# same as above
+for fruit in fruits:
+    print("A fruit of type: %s" % fruit)
+
+# also we can go through ixed lists too
+# notice we have to use %r since we don't konw what's in it
+for i in change:
+    print("I got %r" % i)
+
+# we can also bulid lists. first start with an empty one
+elements = []
+
+# then use the range funtion to do 0 to 5 counts
+for i in range(0,6):
+    print("Adding %d to the list." % i)
+    # append is a function that lists understand
+    elements.append(i)
+
+# 또는 python 에서는 아래와 같이 할 수도 있다
+# list compreehension
+elements2 = [i for i in range(0, 6)]
+
+# now we can print them out too
+for i in elements:
+    print("Element was: %d" % i)
+
+
